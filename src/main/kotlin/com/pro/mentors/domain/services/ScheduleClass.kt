07f32e0e77f1cf814cc3a3bfed33fd7d43eda82c
Emerson Pereira <edap.diego@gmail.com>
2022-01-20 11:53:39 -0300

    first commits scope schedule

--- src/main/kotlin/com/pro/mentors/domain/services/ScheduleClass.kt
+++ src/main/kotlin/com/pro/mentors/domain/services/ScheduleClass.kt
@@ -1,5 +1,18 @@
 package com.pro.mentors.domain.services
 
+import com.pro.mentors.domain.entities.Schedule
+import com.pro.mentors.domain.entities.EventType.BLOCK
+import java.time.LocalDateTime
+import kotlin.time.Duration.Companion.minutes
+
 class ScheduleClass {
+    fun create(schedule: Schedule): Schedule {
+        return schedule
+
+    }
+
+
 
 }
+
+
